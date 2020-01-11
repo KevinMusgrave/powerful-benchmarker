db2980b85dedeceb417072506340a6d8d1c91166
TakeshiMusgrave <tkm45@cornell.edu>
2020-01-11 14:20:20 -0500

    Fixed iteration bug in bayesian optimization. Also made it more resilient to file writing failures

--- run_bayesian_optimization.py
+++ run_bayesian_optimization.py
@@ -55,17 +55,37 @@ def run_experiment(config_foldernames, parameters):
     YR.args.place_to_save_configs = "%s/%s" % (YR.args.experiment_folder, "configs")
     return run.run(YR.args)
 
-def get_log_path(root_experiment_folder):
-    return "%s/bayesian_optimization_logs.json"%(root_experiment_folder)
+def get_log_folder(root_experiment_folder):
+    return "%s/bayesian_optimizer_logs/"%root_experiment_folder
+
+def get_all_log_paths(root_experiment_folder):
+    return sorted(glob.glob("%s/log*.json"%get_log_folder(root_experiment_folder)), reverse=True)
+
+def save_new_log(ax_client, root_experiment_folder):
+    log_paths = get_all_log_paths(root_experiment_folder)
+    log_folder = get_log_folder(root_experiment_folder)
+    c_f.makedir_if_not_there(log_folder)
+    new_log_path = "%s/log%05d.json"%(log_folder, len(log_paths))
+    ax_client.save_to_json_file(filepath=new_log_path)
+
+def open_log(log_paths):
+    for L in log_paths:
+        try:
+            ax_client = AxClient.load_from_json_file(filepath=L)
+            break
+        except:
+            ax_client = None
+    return ax_client
 
 def get_ax_client(YR, bayes_params):
-    log_path = get_log_path(YR.args.root_experiment_folder)
-    if os.path.isfile(log_path):
-        ax_client = AxClient.load_from_json_file(filepath=log_path)
-    else:
+    log_paths = get_all_log_paths(YR.args.root_experiment_folder)
+    ax_client = None
+    if len(log_paths) > 0:
+        ax_client = open_log(log_paths)
+    if ax_client is None:
         ax_client = AxClient()
         ax_client.create_experiment(parameters=bayes_params, name=YR.args.experiment_name, minimize=False, objective_name=YR.args.eval_metric_for_best_epoch)
-    return ax_client, log_path
+    return ax_client
 
 def plot_progress(ax_client, root_experiment_folder, experiment_name):
     html_elements = []
@@ -82,19 +102,18 @@ if __name__ == "__main__":
                           "config_loss_and_miners", "config_transforms", "config_eval"]
 
     YR, bayes_params = read_yaml_and_find_bayes(config_foldernames)
-    ax_client, log_path = get_ax_client(YR, bayes_params)
+    ax_client = get_ax_client(YR, bayes_params)
     num_explored_points = len(ax_client.experiment.trials) if ax_client.experiment.trials else 0
-    n_iter = YR.args.bayesian_optimization_n_iter - num_explored_points
 
-    for i in range(num_explored_points, n_iter):
+    for i in range(num_explored_points, YR.args.bayesian_optimization_n_iter):
         logging.info("Optimization iteration %d"%i)
         parameters, trial_index = ax_client.get_next_trial()
         ax_client.complete_trial(trial_index=trial_index, raw_data=run_experiment(config_foldernames, parameters))
-        ax_client.save_to_json_file(filepath=log_path)
+        save_new_log(ax_client, YR.args.root_experiment_folder)
         plot_progress(ax_client, YR.args.root_experiment_folder, YR.args.experiment_name)
 
-
     logging.info("DONE BAYESIAN OPTIMIZATION")
+    plot_progress(ax_client, YR.args.root_experiment_folder, YR.args.experiment_name)
     best_parameters, best_values = ax_client.get_best_parameters()
     best_parameters_dict = {"best_parameters": best_parameters, "means": best_values[0]}
     c_f.write_yaml("%s/best_parameters.yaml"%YR.args.root_experiment_folder, best_parameters_dict, open_as='w')
