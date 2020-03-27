e66cfd60c8f68354b73ad10e54a67bbf1317a7f3
TakeshiMusgrave <tkm45@cornell.edu>
2020-03-26 20:05:16 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="powerful-benchmarker",
-    version="0.9.12",
+    version="0.9.13",
     author="Kevin Musgrave",
     author_email="tkm45@cornell.edu",
     description="A highly-configurable tool that enables thorough evaluation of deep metric learning algorithms. ",
