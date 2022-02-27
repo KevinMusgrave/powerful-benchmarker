aa1ababa35e642105846e1f0a5109d7693e201a4
TakeshiMusgrave <tkm45@cornell.edu>
2022-02-27 11:30:57 -0500

    Remove summary.txt from upload_logs.hs

--- scripts/upload_logs.sh
+++ scripts/upload_logs.sh
@@ -3,16 +3,12 @@ while :
 do
     cd "$5"
     echo "Getting progress"
-    python summary.py --root_experiment_folder "$1" --save_to_file summary.txt
-    summary_fail=$?
     python print_progress.py --root_experiment_folder "$1" --save_to_file progress.txt
     progress_fail=$?
-    if [ "$summary_fail" = "1" ] && [ "$progress_fail" = "1" ];
+    if [ "$progress_fail" = "1" ];
         then
             echo "fail!"
         else
-            echo "Moving summary.txt"
-            mv summary.txt "$1"    
             echo "Moving progress.txt"
             mv progress.txt "$1"
             cd "$1"
@@ -20,8 +16,8 @@ do
             rm all_logs.zip csvs.zip
             echo "Zipping files"
             find -maxdepth 4 -name *.err -newermt '5 hours ago' -o -name *.out -newermt '5 hours ago' | zip -qr all_logs -@
-            find -maxdepth 4 -name trials.csv -o -name score_vs_test_accuracy.csv -o -name reproduction_score_vs_test_accuracy.csv -o -name study.pkl | zip -qr csvs -@
-            zip -qu csvs.zip summary.txt progress.txt
+            find -maxdepth 4 -name trials.csv | zip -qr csvs -@
+            zip -qu csvs.zip progress.txt
             echo "Deleting existing files"
             gdrive list -q "'$3' in parents" --no-header --max 0 | cut -d" " -f1 - | xargs -L 1 gdrive delete
             echo "Starting upload"
