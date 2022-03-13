f11385af04d7e5dc99c3fb7f96709521d8c2845b
TakeshiMusgrave <tkm45@cornell.edu>
2022-03-12 23:38:27 -0500

    Add office31_amazon to script

--- exp_scripts/office31_jobs.sh
+++ exp_scripts/office31_jobs.sh
@@ -1,1 +1,2 @@
 python launch_multiple.py --exp_config office31/office31_fl6_adam_lr1 --slurm_config a100
+python launch_multiple.py --exp_config office31/office31_amazon_fl6_adam_lr1 --slurm_config a100
