bbacbb98f08a4f676fc6e0d872ae7474af0c93c3
TakeshiMusgrave <tkm45@cornell.edu>
2022-03-12 23:35:54 -0500

    Change num epochs for office31 amazon to 200

--- yaml_creator_helper.py
+++ yaml_creator_helper.py
@@ -4,40 +4,10 @@ import string
 
 def main(cfg):
     base_string = """
-- - python launch_one.py --src_domains art --target_domains clipart
+- - python launch_one.py --src_domains dslr --target_domains amazon
   - *A
   - *common
-- - python launch_one.py --src_domains art --target_domains product
-  - *A
-  - *common
-- - python launch_one.py --src_domains art --target_domains real
-  - *A
-  - *common
-- - python launch_one.py --src_domains clipart --target_domains art
-  - *A
-  - *common
-- - python launch_one.py --src_domains clipart --target_domains product
-  - *A
-  - *common
-- - python launch_one.py --src_domains clipart --target_domains real
-  - *A
-  - *common
-- - python launch_one.py --src_domains product --target_domains art
-  - *A
-  - *common
-- - python launch_one.py --src_domains product --target_domains clipart
-  - *A
-  - *common
-- - python launch_one.py --src_domains product --target_domains real
-  - *A
-  - *common
-- - python launch_one.py --src_domains real --target_domains art
-  - *A
-  - *common
-- - python launch_one.py --src_domains real --target_domains product
-  - *A
-  - *common
-- - python launch_one.py --src_domains real --target_domains clipart
+- - python launch_one.py --src_domains webcam --target_domains amazon
   - *A
   - *common
 """
