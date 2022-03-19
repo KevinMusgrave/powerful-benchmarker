7030ae3262c6edc9413b107b70c08f040efef143
TakeshiMusgrave <tkm45@cornell.edu>
2022-03-19 05:52:54 -0400

    Move upload_logs to root leveL

--- upload_logs.py
+++ upload_logs.py
@@ -10,7 +10,7 @@ from powerful_benchmarker.utils.constants import add_default_args
 def main(cfg):
     curr_dir = os.getcwd()
 
-    command = "bash -i ./powerful_benchmarker/scripts/upload_logs.sh {0} {1} {2} {3} {4} {5}".format(
+    command = "bash -i ./scripts/upload_logs.sh {0} {1} {2} {3} {4} {5}".format(
         cfg.exp_folder,
         cfg.slurm_folder,
         cfg.gdrive_folder,
