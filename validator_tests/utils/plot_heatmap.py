c26bec48b90ada2f472c613f722e79402a968875
TakeshiMusgrave <tkm45@cornell.edu>
2022-03-26 05:30:35 -0400

    plot heatmap vs thresholds

--- validator_tests/utils/plot_heatmap.py
+++ validator_tests/utils/plot_heatmap.py
@@ -7,13 +7,13 @@ from pytorch_adapt.utils import common_functions as c_f
 from .df_utils import unify_validator_columns
 
 
-def plot_heatmap(df, plots_folder, filename):
+def plot_fn(df, plots_folder, filename, index, columns, values):
     sns.set(rc={"figure.figsize": (20, 15)})
     plots_folder = os.path.join(plots_folder, "heatmaps")
     c_f.makedir_if_not_there(plots_folder)
 
-    df = df[["validator", "adapter", "correlation"]]
-    df = df.pivot("validator", "adapter", "correlation")
+    df = df[[index, columns, values]]
+    df = df.pivot(index, columns, values)
 
     plot = sns.heatmap(data=df, mask=df.isnull(), cmap="viridis", vmin=-1, vmax=1)
     fig = plot.get_figure()
@@ -24,17 +24,47 @@ def plot_heatmap(df, plots_folder, filename):
     fig.clf()
 
 
-def plot_heatmap_per_adapter(df, plots_folder):
+def process_df(df):
     df = df[df["validator"] != "Accuracy"]
-    df = unify_validator_columns(df)
+    return unify_validator_columns(df)
+
+
+def plot_heatmap_per_adapter(df, plots_folder):
+    df = process_df(df)
     print("plot_heatmap_per_adapter")
     for x in tqdm.tqdm([-0.01, 0.5, 0.9, 0.98, 0.99]):
         filename = f"heatmap_{x:.2f}.png"
-        plot_heatmap(df[df["src_threshold"] == x], plots_folder, filename)
+        plot_fn(
+            df[df["src_threshold"] == x],
+            plots_folder,
+            filename,
+            "validator",
+            "adapter",
+            "correlation",
+        )
 
     df = (
         df.groupby(["validator", "adapter"])["correlation"]
         .mean()
         .reset_index(name="correlation")
     )
-    plot_heatmap(df, plots_folder, "heatmap_avg_correlation.png")
+    plot_fn(
+        df,
+        plots_folder,
+        "heatmap_avg_correlation.png",
+        "validator",
+        "adapter",
+        "correlation",
+    )
+
+
+def plot_heatmap(df, plots_folder):
+    df = process_df(df)
+    plot_fn(
+        df,
+        plots_folder,
+        "heatmap_global.png",
+        "validator",
+        "src_threshold",
+        "correlation",
+    )
