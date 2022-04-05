4aff6b84172001123fd35e3a71c4fc11a1ca0c38
TakeshiMusgrave <tkm45@cornell.edu>
2022-04-05 18:28:56 -0400

    Updated configs

--- powerful_benchmarker/exp_scripts/officehome_jobs.sh
+++ powerful_benchmarker/exp_scripts/officehome_jobs.sh
@@ -1,2 +1,1 @@
-# python powerful_benchmarker/launch_multiple.py --exp_config officehome/officehome_fl6_adam_lr1 --slurm_config a100
-python powerful_benchmarker/launch_multiple.py --exp_config test/test_jobs --slurm_config a100
+python powerful_benchmarker/launch_multiple.py --exp_config officehome/officehome_fl3_adam_lr1 --slurm_config a100
