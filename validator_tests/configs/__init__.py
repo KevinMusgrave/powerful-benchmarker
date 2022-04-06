284d4144732538360a056ab71ee68e59f5d66492
TakeshiMusgrave <tkm45@cornell.edu>
2022-04-06 09:39:38 -0400

    Deleted IST configs

--- validator_tests/configs/__init__.py
+++ validator_tests/configs/__init__.py
@@ -10,7 +10,6 @@ from .d_logits_accuracy_config import DLogitsAccuracy
 from .dev_config import DEV, DEVBinary
 from .diversity_config import Diversity
 from .entropy_config import Entropy
-from .ist_config import IST
 from .knn_config import KNN, TargetKNN, TargetKNNLogits
 from .mmd_config import MMD, MMDFixedB, MMDPerClass, MMDPerClassFixedB
 from .snd_config import SND
--- validator_tests/flags/__init__.py
+++ validator_tests/flags/__init__.py
@@ -10,7 +10,6 @@ from .dev import DEV, DEVBinary
 from .diversity import Diversity
 from .dlogits_accuracy import DLogitsAccuracy
 from .entropy import Entropy
-from .ist import IST
 from .knn import KNN, TargetKNN, TargetKNNLogits
 from .mmd import MMD, MMDFixedB, MMDPerClass, MMDPerClassFixedB
 from .snd import SND
