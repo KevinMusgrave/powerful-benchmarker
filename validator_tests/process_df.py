78e3cbdd3413c3365a8aed7a9cbaccb187fc82ee
TakeshiMusgrave <tkm45@cornell.edu>
2022-04-06 12:38:20 -0400

    Added warn_unfinished_validators

--- validator_tests/process_df.py
+++ validator_tests/process_df.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import os
 import sys
 
@@ -16,7 +17,10 @@ from validator_tests.utils.df_utils import (
     get_all_acc,
     print_validators_with_nan,
     remove_nan_scores,
+    unify_validator_columns,
 )
+
+EXPECTED_NUMBER_OF_CHECKPOINTS = 10 * 100 * 20
 
 
 def read_all_dfs(exp_folder):
@@ -31,16 +35,37 @@ def process_acc_validator(df):
     return df
 
 
+def warn_unfinished_validators(df):
+    df = unify_validator_columns(df)
+    unfinished = {}
+    for v in df["validator"].unique():
+        num_done = len(df[df["validator"] == v])
+        if num_done < EXPECTED_NUMBER_OF_CHECKPOINTS:
+            unfinished[v] = num_done
+    if len(unfinished) > 0:
+        print("WARNING: the following validators haven't finished")
+        print(json.dumps(unfinished, indent=4, sort_keys=True))
+
+
 def main(args):
     exp_folder = os.path.join(args.exp_folder, args.exp_group)
     filename = os.path.join(exp_folder, PROCESSED_DF_FILENAME)
+
+    print("reading file")
     df = read_all_dfs(exp_folder)
     convert_list_to_tuple(df)
+
+    print("adding derived scores")
     df = derive.add_derived_scores(df)
+    print("finding unfinished validators")
+    warn_unfinished_validators(df)
+
+    print("processing accuracies")
     df = process_acc_validator(df)
     print_validators_with_nan(df)
     df = remove_nan_scores(df)
     print_validators_with_nan(df, assert_none=True)
+
     print(f"processed df:\n{df}")
     df.to_pickle(filename)
 
