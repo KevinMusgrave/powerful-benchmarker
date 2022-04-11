adb5e0e53c2f1c44a8823f796bb9f1ecc108e8e6
KevinMusgrave <tkm45@cornell.edu>
2022-04-11 16:34:52 -0400

    updated configs

--- validator_tests/scripts/officehome.py
+++ validator_tests/scripts/officehome.py
@@ -1,18 +1,26 @@
 import subprocess
 
 components = [
-    {"flags": "Accuracy", "exp_per_slurm_job": "20", "trials_per_exp": "100"},
-    {"flags": "Entropy", "exp_per_slurm_job": "20", "trials_per_exp": "100"},
-    {"flags": "Diversity", "exp_per_slurm_job": "20", "trials_per_exp": "100"},
+    # {"flags": "Accuracy", "exp_per_slurm_job": "20", "trials_per_exp": "100"},
+    # {"flags": "Entropy", "exp_per_slurm_job": "20", "trials_per_exp": "100"},
+    # {"flags": "Diversity", "exp_per_slurm_job": "20", "trials_per_exp": "100"},
     {"flags": "DEV", "exp_per_slurm_job": "20", "trials_per_exp": "100"},
-    {"flags": "SND", "exp_per_slurm_job": "15", "trials_per_exp": "100"},
-    # {"flags": "KNN", "exp_per_slurm_job": "20", "trials_per_exp": "100"},
+    {"flags": "DEVBinary", "exp_per_slurm_job": "20", "trials_per_exp": "100"},
+    # {"flags": "SND", "exp_per_slurm_job": "15", "trials_per_exp": "100"},
+    {"flags": "DomainCluster", "exp_per_slurm_job": "20", "trials_per_exp": "100"},
+    {
+        "flags": "ClassAMICentroidInit",
+        "exp_per_slurm_job": "20",
+        "trials_per_exp": "100",
+    },
+    {"flags": "MMD", "exp_per_slurm_job": "20", "trials_per_exp": "100"},
+    {"flags": "BNM", "exp_per_slurm_job": "20", "trials_per_exp": "100"},
 ]
 
 exp_names = ["atdoc", "bnm", "bsp", "cdan", "dann", "gvb", "im", "mcc", "mcd", "mmd"]
 exp_names = " ".join(exp_names)
 
 for x in components:
-    command = "python validator_tests/run_validators.py --exp_group_prefix officehome_ --slurm_config a100 --run"
+    command = "python validator_tests/run_validators.py --exp_group_prefix officehome --exp_group_contains fl6 --slurm_config a100 --run"
     command += f" --exp_names {exp_names} --flags {x['flags']} --exp_per_slurm_job {x['exp_per_slurm_job']} --trials_per_exp {x['trials_per_exp']}"
     subprocess.run(command.split(" "))
