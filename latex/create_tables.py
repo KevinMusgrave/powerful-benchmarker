f8d84fd1582ed3ca7e364841d05f9e1c8d75efa9
KevinMusgrave <tkm45@cornell.edu>
2022-04-24 02:40:33 -0400

    Save latex to text file

--- latex/create_tables.py
+++ latex/create_tables.py
@@ -3,10 +3,23 @@ import os
 import sys
 
 import pandas as pd
+from pytorch_adapt.utils import common_functions as c_f
 
 sys.path.insert(0, ".")
 from validator_tests.utils import utils
-from validator_tests.utils.constants import add_exp_group_args
+from validator_tests.utils.constants import add_exp_group_args, get_name_from_exp_groups
+
+
+def save_to_latex(df, folder, filename):
+    c_f.makedir_if_not_there(folder)
+    df_style = df.style.highlight_max(props="textbf:--rwrap")
+    latex_str = df_style.format(escape="latex", na_rep="-",).to_latex(
+        hrules=True,
+        position_float="centering",
+    )
+    full_path = os.path.join(folder, f"{filename}.txt")
+    with open(full_path, "w") as text_file:
+        text_file.write(latex_str)
 
 
 def convert_adapter_name(df):
@@ -58,13 +71,17 @@ def main(args):
 
     df = postprocess_df(df)
     print(df)
+    output_folder = os.path.join(
+        args.output_folder, get_name_from_exp_groups(exp_groups)
+    )
+    save_to_latex(df, output_folder, args.filename)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(allow_abbrev=False)
     add_exp_group_args(parser)
     parser.add_argument("--input_folder", type=str, default="tables")
-    parser.add_argument("--output_folder", type=str, default="latex_tables")
+    parser.add_argument("--output_folder", type=str, default="tables_latex")
     parser.add_argument("--filename", type=str, required=True)
     args = parser.parse_args()
     main(args)
