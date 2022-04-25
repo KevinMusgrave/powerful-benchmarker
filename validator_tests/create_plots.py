8d00da9c0f5b96952e9d82c778833c6c5bc94ec3
KevinMusgrave <tkm45@cornell.edu>
2022-04-25 03:20:57 -0400

    Use get_name_from_df in create_plots

--- validator_tests/create_plots.py
+++ validator_tests/create_plots.py
@@ -5,11 +5,8 @@ import sys
 sys.path.insert(0, ".")
 from powerful_benchmarker.utils.constants import add_default_args
 from validator_tests.utils import create_main
-from validator_tests.utils.constants import (
-    add_exp_group_args,
-    get_name_from_exp_groups,
-    get_per_src_threshold_df,
-)
+from validator_tests.utils.constants import add_exp_group_args
+from validator_tests.utils.df_utils import get_name_from_df, get_per_src_threshold_df
 from validator_tests.utils.plot_heatmap import (
     plot_heatmap,
     plot_heatmap_average_across_adapters,
@@ -28,16 +25,21 @@ def scatter_and_heatmap(exp_folder, exp_groups, plots_folder, df, per_feature_la
             validator_set=args.scatter_plot_validator_set,
         )
     if args.heatmap:
-        plots_folder = os.path.join(plots_folder, get_name_from_exp_groups(exp_groups))
         per_src = get_per_src_threshold_df(exp_folder, False, args.topN, exp_groups)
-        plot_heatmap(per_src, plots_folder, args.topN)
+        curr_plots_folder = os.path.join(
+            plots_folder, get_name_from_df(per_src, assert_one_task=True)
+        )
+        plot_heatmap(per_src, curr_plots_folder, args.topN)
 
         per_src = get_per_src_threshold_df(
             exp_folder, True, args.topN_per_adapter, exp_groups
         )
-        plot_heatmap_per_adapter(per_src, plots_folder, args.topN_per_adapter)
+        curr_plots_folder = os.path.join(
+            plots_folder, get_name_from_df(per_src, assert_one_task=True)
+        )
+        plot_heatmap_per_adapter(per_src, curr_plots_folder, args.topN_per_adapter)
         plot_heatmap_average_across_adapters(
-            per_src, plots_folder, args.topN_per_adapter
+            per_src, curr_plots_folder, args.topN_per_adapter
         )
 
 
