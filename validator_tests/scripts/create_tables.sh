c6a068abf386e9635cc06952076f2482eca242ab
KevinMusgrave <tkm45@cornell.edu>
2022-05-15 01:11:35 -0400

    Add create_tables.sh

---
+++ validator_tests/scripts/create_tables.sh
@@ -0,0 +1,6 @@
+python validator_tests/create_tables.py --exp_group_prefix mnist --run_single --run_combined
+python validator_tests/create_tables.py --exp_group_prefix mnist --run_single --run_combined --topN 1 --topN_per_adapter 1
+python validator_tests/create_tables.py --exp_group_prefix office31 --run_single --run_combined
+python validator_tests/create_tables.py --exp_group_prefix office31 --run_single --run_combined --topN 1 --topN_per_adapter 1
+python validator_tests/create_tables.py --exp_group_prefix officehome --run_single --run_combined
+python validator_tests/create_tables.py --exp_group_prefix officehome --run_single --run_combined --topN 1 --topN_per_adapter 1
