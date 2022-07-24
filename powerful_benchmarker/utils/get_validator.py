61f8e6eb286516353670802635fd0f4f45b0fe2d
KevinMusgrave <tkm45@cornell.edu>
2022-07-24 05:14:08 -0400

    Removed old code from get_validator

--- powerful_benchmarker/utils/get_validator.py
+++ powerful_benchmarker/utils/get_validator.py
@@ -44,31 +44,6 @@ def get_validator(
         validator = target_accuracy(num_classes, average="micro")
     elif validator_name == "src_accuracy":
         validator = src_accuracy(num_classes)
-    elif validator_name == "entropy_diversity":
-        layer = "preds" if feature_layer == 8 else "logits"
-        validator = MultipleValidators(
-            validators={
-                "entropy": EntropyValidator(layer=layer),
-                "diversity": DiversityValidator(layer=layer),
-            },
-        )
-    elif validator_name == "SND":
-        validator = SNDValidator()
-    elif validator_name in ["DEV"]:
-        if feature_layer == 8:
-            error_fn = NLLLoss(reduction="none")
-            error_layer = "preds"
-        else:
-            error_fn = torch.nn.CrossEntropyLoss(reduction="none")
-            error_layer = "logits"
-        temp_folder = os.path.join(checkpoint_path, "val_temp_folder")
-        validator = DeepEmbeddedValidator(
-            temp_folder=temp_folder,
-            num_workers=0,
-            batch_size=256,
-            error_fn=error_fn,
-            error_layer=error_layer,
-        )
 
     validator = ScoreHistory(validator, ignore_epoch=0)
     checkpoint_fn = CheckpointFnCreator(dirname=checkpoint_path, require_empty=False)
