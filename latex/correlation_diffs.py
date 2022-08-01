32ba300cc3e60cc969ef014e4f643c0ea18b7091
KevinMusgrave <tkm45@cornell.edu>
2022-08-01 15:44:02 -0400

    Include spearman and weighted_spearman in correlation_diffs

--- latex/correlation_diffs.py
+++ latex/correlation_diffs.py
@@ -1,3 +1,5 @@
+import os
+
 from latex.correlation import get_postprocess_df, get_preprocess_df
 from latex.table_creator import table_creator
 
@@ -8,7 +10,7 @@ def correlation_diffs(args, per_adapter, names):
         per_adapter_str = "_per_adapter" if per_adapter else ""
         basename = f"{name}{per_adapter_str}"
 
-        df, _ = table_creator(
+        df, output_folder = table_creator(
             args,
             basename,
             preprocess_df=get_preprocess_df(per_adapter),
@@ -16,12 +18,15 @@ def correlation_diffs(args, per_adapter, names):
             do_save_to_latex=False,
         )
 
+        df = (
+            df.drop(columns=["Mean", "Std"])
+            .reset_index()
+            .melt(id_vars=["level_0", "level_1"], var_name="task", value_name=name)
+        )
+
         dfs.append(df)
 
-    df = dfs[0] - dfs[1]
-    df = (
-        df.drop(columns=["Mean", "Std"])
-        .reset_index()
-        .melt(id_vars=["level_0", "level_1"])
-    )
-    print(df.sort_values("value"))
+    df = dfs[0].merge(dfs[1], on=["level_0", "level_1", "task"])
+    df["diff"] = df["weighted_spearman"] - df["spearman"]
+    df = df.sort_values("diff")
+    df.to_csv(os.path.join(output_folder, "correlation_diffs.csv"))
