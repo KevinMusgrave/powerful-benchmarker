058dd9a7f87f203385865f7f646be0052c0d6523
KevinMusgrave <tkm45@cornell.edu>
2022-08-01 03:55:11 -0400

    Remove unused function

--- validator_tests/utils/trndcg.py
+++ validator_tests/utils/trndcg.py
@@ -10,17 +10,6 @@ def set_nan_inf_to_min(x):
     x[~is_finite] = np.min(x[is_finite])
 
 
-def special_min_max_normalize(x):
-    set_nan_inf_to_min(x)
-    max_min = max(x) - min(x)
-    if max_min == 0:
-        max_x = max(x)
-        if max_x == 0:
-            return x + 1
-        return x / max_x
-    return (x - min(x)) / max_min
-
-
 def weighted_spearman(target_accuracies, validation_scores, pow):
     set_nan_inf_to_min(validation_scores)
     assert np.isfinite(target_accuracies).all()
