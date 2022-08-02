b9b79ada607b94a8193250ae95c572ba51323865
KevinMusgrave <tkm45@cornell.edu>
2022-08-02 17:24:45 -0400

    Updated create_plots.sh

--- validator_tests/scripts/create_plots.sh
+++ validator_tests/scripts/create_plots.sh
@@ -1,1 +1,13 @@
-python validator_tests/create_plots.py --scatter --scatter_plot_validator_set DEVBinary --run_combined --exp_group_prefix officehome
+exp_prefixes=("mnist_mnist_mnistm" "office31_amazon_dslr" "office31_amazon_webcam" "office31_dslr_amazon" \
+"office31_dslr_webcam" "office31_webcam_amazon" "office31_webcam_dslr" \
+"officehome_art_clipart" "officehome_art_product" "officehome_art_real" \
+"officehome_clipart_art" "officehome_clipart_product" "officehome_clipart_real" \
+"officehome_product_art" "officehome_product_clipart" "officehome_product_real" \
+"officehome_real_art" "officehome_real_clipart" "officehome_real_product")
+
+
+for i in "${exp_prefixes[@]}"
+do
+    python simple_slurm.py --command "python validator_tests/create_plots.py --no_color --validator_set BNMSummedSrcVal --adapter ATDOCConfig --run_combined --per_adapter" --all_in_one --exp_group_prefix $i --exp_group_excludes oracle \
+    --slurm_config_folder validator_tests --slurm_config mnist --job_name=eval_validators --cpus-per-task=4
+done
