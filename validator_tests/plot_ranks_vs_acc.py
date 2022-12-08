232638e4d7b6ed37f39ed37223ee84e8a9cc7c87
KevinMusgrave <tkm45@cornell.edu>
2022-12-08 18:44:34 -0500

    Use mean_acc as x-axis

--- validator_tests/plot_ranks_vs_acc.py
+++ validator_tests/plot_ranks_vs_acc.py
@@ -54,10 +54,18 @@ def plot_corr_vs_acc(df, max_rank, corr_name, folder, filename):
     to_plot = df[df["rank"] <= max_rank]
     to_plot = to_plot.sort_values(by=["adapter"])
     to_plot["adapter"] = to_plot["adapter"].str.replace("Config", "")
-    for x in [corr_name, "adapter"]:
+    to_plot["mean_acc"] = to_plot.groupby(["adapter", "rank_type"])[
+        TARGET_ACCURACY
+    ].transform("mean")
+    for x in [corr_name, "adapter", "mean_acc"]:
+        _to_plot = (
+            to_plot[to_plot["rank_type"] == "by validation score"]
+            if x == "mean_acc"
+            else to_plot
+        )
         sns.set(style="whitegrid", rc={"figure.figsize": (8, 8)})
         plot = sns.scatterplot(
-            data=to_plot, x=x, y=TARGET_ACCURACY, hue="rank_type", alpha=0.5
+            data=_to_plot, x=x, y=TARGET_ACCURACY, hue="rank_type", alpha=0.5
         )
         sns.move_legend(plot, "upper left", bbox_to_anchor=(1, 1))
         fig = plot.get_figure()
