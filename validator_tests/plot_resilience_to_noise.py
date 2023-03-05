e40260b00b93bc1592a8d4eb034759b9fb003a89
KevinMusgrave <tkm45@cornell.edu>
2023-03-05 11:12:04 -0500

    Better axis title

--- validator_tests/plot_resilience_to_noise.py
+++ validator_tests/plot_resilience_to_noise.py
@@ -8,8 +8,12 @@ import seaborn as sns
 def save_plot(output_folder, df):
     Ns = [100, 10, 1]
     x_axis_title = "Standard deviation of noise added to target-domain accuracy"
-    keys = [f"Top {N} Accuracy" for N in Ns] + ["Noise Standard Deviation"]
-    new_keys = [f"Avg Accuracy of Top {N}" for N in Ns] + [x_axis_title]
+    y_axis_title = "Correlation with 0-noise ranking"
+    keys = [f"Top {N} Accuracy" for N in Ns] + [
+        "Noise Standard Deviation",
+        "Correlation with original data",
+    ]
+    new_keys = [f"Avg Accuracy of Top {N}" for N in Ns] + [x_axis_title, y_axis_title]
     df = df.rename(columns={k: v for k, v in zip(keys, new_keys)})
 
     df = pd.melt(
@@ -17,17 +21,17 @@ def save_plot(output_folder, df):
         id_vars=[x_axis_title],
         value_vars=[
             "Weighted Spearman Correlation",
-            *new_keys,
+            *new_keys[:-2],
         ],
         var_name="Metric",
-        value_name="Correlation with original data",
+        value_name=y_axis_title,
     )
 
     sns.set(font_scale=1, style="whitegrid", rc={"figure.figsize": (8, 8)})
     plot = sns.lineplot(
         data=df,
         x=x_axis_title,
-        y="Correlation with original data",
+        y=y_axis_title,
         hue="Metric",
     )
     sns.move_legend(plot, "lower left")
